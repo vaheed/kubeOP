3f2bb8e58e2fb694bda40dda54ae40348e5278e3
vaheeD khoshnouD <khoshnud@gmail.com>
2025-10-12 13:58:18 +0330

    Format version test

--- testcase/version_test.go
+++ testcase/version_test.go
@@ -6,7 +6,7 @@ import (
 )
 
 func TestVersion_Bumped(t *testing.T) {
-        if version.Version != "0.3.2" {
-                t.Fatalf("expected version 0.3.2, got %q", version.Version)
-        }
+	if version.Version != "0.3.2" {
+		t.Fatalf("expected version 0.3.2, got %q", version.Version)
+	}
 }
