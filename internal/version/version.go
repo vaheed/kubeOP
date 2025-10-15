eb8db3b2277928dfb87dc569f1bcc499990a8cfc
vaheeD khoshnouD <khoshnud@gmail.com>
2025-10-15 12:59:52 +0330

    Format version metadata

--- internal/version/version.go
+++ internal/version/version.go
@@ -2,7 +2,7 @@ package version
 
 // Default build metadata; overridden via -ldflags when the binary is built.
 var (
-        Version = "0.8.10"
-        Commit  = ""
-        Date    = ""
+	Version = "0.8.10"
+	Commit  = ""
+	Date    = ""
 )
