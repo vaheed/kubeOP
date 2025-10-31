e34b446f54f05e9398be0edc35db4b3bbb6aece9
vaheeD <khoshnud@gmail.com>
2025-10-31 10:59:44 +0330

    refactor: remove unused import for types in admission main package

--- cmd/admission/main.go
+++ cmd/admission/main.go
@@ -17,7 +17,6 @@ import (
     corev1 "k8s.io/api/core/v1"
     arv1 "k8s.io/api/admissionregistration/v1"
     metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-    "k8s.io/apimachinery/pkg/types"
     "k8s.io/client-go/kubernetes"
     "k8s.io/client-go/rest"
 
